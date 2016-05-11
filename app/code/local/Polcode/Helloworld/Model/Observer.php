a84af009cbc14323f9bffb0e3d252ae9d200ad9e
robmro27 <robert.mroz1985@gmail.com>
2016-05-11 09:46:03 +0200

    try catch observer

--- app/code/local/Polcode/Helloworld/Model/Observer.php
+++ app/code/local/Polcode/Helloworld/Model/Observer.php
@@ -4,32 +4,39 @@ class Polcode_Helloworld_Model_Observer {
     
     public function checkProduct( $observer )
     {
-        
-        // check if module is enabled
-        if (!Mage::helper('helloworld/helper')->isEnabled()) {
-            return $this;
-        }
-        
-        // get sku from system settings
-        $sku = Mage::getStoreConfig('polcode_section/product/sku');
-        
-        // check
-        foreach ($observer->getEvent()->getOrder()->getAllItems() as $item)
-        {
+        try {
             
-            if ( strtolower($sku) == strtolower($item->getProduct()['sku']) ) {
-                
-                $product = Mage::getModel('helloworld/product');
-                $product->setSku($item->getProduct()['sku']);
-                $product->setProductId($item->getProduct()['entity_id']);
-                $product->setProductName($item->getProduct()['name']);
-                $product->setPrice($item->getProduct()['price']);
-                $product->setQty($observer->getEvent()->getOrder()['total_qty_ordered']);
-                $product->save();
-                
-                break;
-                
+            throw new \Exception('Test');
+            
+            // check if module is enabled
+            if (!Mage::helper('helloworld/helper')->isEnabled()) {
+                return $this;
             }
+
+            // get sku from system settings
+            $sku = Mage::getStoreConfig('polcode_section/product/sku');
+
+            // check
+            foreach ($observer->getEvent()->getOrder()->getAllItems() as $item)
+            {
+
+                if ( strtolower($sku) == strtolower($item->getProduct()['sku']) ) {
+
+                    $product = Mage::getModel('helloworld/product');
+                    $product->setSku($item->getProduct()['sku']);
+                    $product->setProductId($item->getProduct()['entity_id']);
+                    $product->setProductName($item->getProduct()['name']);
+                    $product->setPrice($item->getProduct()['price']);
+                    $product->setQty($observer->getEvent()->getOrder()['total_qty_ordered']);
+                    $product->save();
+
+                    break;
+
+                }
+            }
+        } catch (\Exception $ex) 
+        {
+            Mage::log('Error in event: ' . __METHOD__ . " Error: " . $ex->getMessage(), null, 'event.log' , true);
         }
      
         return $this;
